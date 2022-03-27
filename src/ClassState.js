8888e884ab32484393e3585b6db2074a6f94217e
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-26 21:26:42 -0400

    Agragando estado de error  los componentes

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,13 +1,25 @@
 import React from "react";
 
 class ClassState extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      error: false
+    }
+  }
   render() {
+
+
     return (
       <div>
         <h2>Eliminar ClassState</h2>
         <p>Por favor, escriba el código de seguridad.</p>
+
+        {this.state.error &&(
+        <p>Error: el codigo es incorrecto</p>
+      )}
         <input type="text" placeholder="código de seguridad" />
-        <button>Comprobar</button>
+        <button  onClick={()=> this.setState(prevState =>({error: !prevState.error }) )}>Comprobar</button>
       </div>
     );
   }
--- src/UseState.js
+++ src/UseState.js
@@ -1,12 +1,18 @@
-import React from "react";
+import React,{useState} from "react";
 
 const UseState = () => {
+
+  const [error, setError] = useState(false);
   return (
     <div>
       <h2>Eliminar UseState</h2>
       <p>Por favor, escriba el código de seguridad.</p>
+
+      {error &&(
+        <p>Error: el codigo es incorrecto</p>
+      )}
       <input type="text" placeholder="código de seguridad" />
-      <button>Comprobar</button>
+      <button onClick={()=> setError(!error)}>Comprobar</button>
     </div>
   );
 };
