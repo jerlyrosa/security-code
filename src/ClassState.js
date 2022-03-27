8012ed81e605c78c4b5ff7cf6fa040377dfbd3cd
sr-jerly <jerlymigueldelarosah@gmail.com>
2022-03-26 22:03:43 -0400

    Agragando efecto de carga en  los componentes useState y ClassState

--- src/ClassState.js
+++ src/ClassState.js
@@ -1,25 +1,56 @@
 import React from "react";
+import { Loading } from "./Loading";
 
 class ClassState extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
-      error: false
+      error: false,
+      loading: false,
+    };
+  }
+
+  // UNSAFE_componentWillMount(){
+  //   console.log('componentWillMount: Inicia antes de renderizar nuestro componentes ');
+
+  // }
+
+  // componentDidMount(){
+  //   console.log('componentDidMount: Se ejecuta cuando se renderiza por primera vez  ');
+
+  // }
+
+  componentDidUpdate() {
+    console.log(
+      "componentDidUpdate: Se ejecuta cuando se actualiza nuestro estado"
+    );
+
+    if (this.state.loading) {
+      console.log("Validando...");
+      setTimeout(() => {
+        console.log("Final de la Validacion");
+
+        this.setState({ loading: false });
+      }, 3000);
     }
   }
+
   render() {
-
-
     return (
       <div>
         <h2>Eliminar ClassState</h2>
         <p>Por favor, escriba el código de seguridad.</p>
 
-        {this.state.error &&(
-        <p>Error: el codigo es incorrecto</p>
-      )}
+        {this.state.error && <p>Error: el codigo es incorrecto</p>}
+
+        {this.state.loading && <Loading />}
+
         <input type="text" placeholder="código de seguridad" />
-        <button  onClick={()=> this.setState(prevState =>({error: !prevState.error }) )}>Comprobar</button>
+        <button
+          onClick={() => this.setState((prevState) => ({ loading: true }))}
+        >
+          Comprobar
+        </button>
       </div>
     );
   }
--- src/UseState.js
+++ src/UseState.js
@@ -1,18 +1,32 @@
-import React,{useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const UseState = () => {
-
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    // console.log("Inicio del efecto");
+
+    if (loading) {
+      // console.log("Validando...");
+      setTimeout(() => {
+        // console.log("Final de la Validacion");
+
+        setLoading(false);
+      }, 3000);
+    }
+    // console.log("Final del efecto");
+  }, [loading]);
   return (
     <div>
       <h2>Eliminar UseState</h2>
       <p>Por favor, escriba el código de seguridad.</p>
 
-      {error &&(
-        <p>Error: el codigo es incorrecto</p>
-      )}
+      {error && <p>Error: el codigo es incorrecto</p>}
+
+      {loading && <p>Cargando...</p>}
       <input type="text" placeholder="código de seguridad" />
-      <button onClick={()=> setError(!error)}>Comprobar</button>
+      <button onClick={() => setLoading(!error)}>Comprobar</button>
     </div>
   );
 };
